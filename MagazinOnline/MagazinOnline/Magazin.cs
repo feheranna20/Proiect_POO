fa229978f136975f7fc2ab7d64f3787cf1afd9ce
Feher Annamaria <annamariafeher03@gmail.com>
2025-01-04 00:36:00 +0200

    adaugare clasa Magazin

---
+++ MagazinOnline/MagazinOnline/Magazin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinOnline
+{
+    public class Magazin
+    {
+        public List<Produs> Produse { get; private set; } = new List<Produs>();
+        public List<Comanda> Comenzi { get; private set; } = new List<Comanda>();
+
+        public void AdaugaProdus(Produs produs)
+        {
+            Produse.Add(produs);
+        }
+    }
+}
