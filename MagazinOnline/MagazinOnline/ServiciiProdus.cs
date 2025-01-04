6ada982ac7d285702b491b352c5011336126998b
Feher Annamaria <annamariafeher03@gmail.com>
2025-01-04 13:08:33 +0200

    Update clasa ServiciiProdus

--- MagazinOnline/MagazinOnline/ServiciiProdus.cs
+++ MagazinOnline/MagazinOnline/ServiciiProdus.cs
@@ -9,5 +9,47 @@ namespace MagazinOnline
     public class ServiciiProdus
     {
         public List<Produs> Products { get; private set; } = new List<Produs>();
+
+        public void AddProduct(Produs product)
+        {
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(string name)
+        {
+            Produs productToRemove = null;
+            foreach (var product in Products)
+            {
+                if (product.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    productToRemove = product;
+                    break;
+                }
+            }
+
+            if (productToRemove != null)
+            {
+                Products.Remove(productToRemove);
+            }
+            else
+            {
+                throw new Exception("Product not found.");
+            }
+        }
+
+        public void UpdateStock(string name, int newStock)
+        {
+            foreach (var product in Products)
+            {
+                if (product.Name == name)
+                {
+                    product.Stock = newStock;
+                    return;
+                }
+            }
+
+            throw new Exception("Product not found.");
+        }
+
     }
 }
