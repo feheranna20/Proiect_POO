6e4dd42cd00ff97210ecf7dd567bc89396951046
Feher Annamaria <annamariafeher03@gmail.com>
2025-01-04 11:41:38 +0200

    adaugare interfata meniu principal

--- MagazinOnline/MagazinOnline/Program.cs
+++ MagazinOnline/MagazinOnline/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Main Menu ===\n");
+                Console.WriteLine("1. User Menu");
+                Console.WriteLine("2. Administrator Menu");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose an option: ");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "0":
+                        
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Press any key to try again.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
     }
 }
