7e5c8bd77de5d6052a666f04ab5571a1ac92d2a4
Feher Annamaria <annamariafeher03@gmail.com>
2025-01-04 12:51:35 +0200

    adaugare clasa SeviciiProdus si ServiciiComanda

---
+++ MagazinOnline/MagazinOnline/ServiciiComanda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinOnline
+{
+    public class ServiciiComanda
+    {
+        public List<Comanda> Orders { get; private set; } = new List<Comanda>();
+    }
+}
---
+++ MagazinOnline/MagazinOnline/ServiciiProdus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinOnline
+{
+    public class ServiciiProdus
+    {
+        public List<Produs> Products { get; private set; } = new List<Produs>();
+    }
+}
