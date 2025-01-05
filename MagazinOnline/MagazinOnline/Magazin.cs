4af0bd7b0ec93ba43f230ff643c5e81508ddc117
andreeaflorea04 <161115965+andreeaflorea04@users.noreply.github.com>
2025-01-05 18:26:18 +0200

    Magazin.cs

--- MagazinOnline/MagazinOnline/Magazin.cs
+++ MagazinOnline/MagazinOnline/Magazin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +15,43 @@ namespace MagazinOnline
         {
             Produse.Add(produs);
         }
+
+        public void ScoateProdus(string name)
+        {
+            var produs = Produse.FirstOrDefault(p => p.Name == name);
+            if (produs != null)
+            {
+                Produse.Remove(produs);
+            }
+            else
+            {
+                throw new Exception("Produsul nu a fost gasit.");
+            }
+        }
+
+        public void ActualizeazaStoc(string name, int newStock)
+        {
+            var produs = Produse.FirstOrDefault(p => p.Name == name);
+            if (produs != null)
+            {
+                produs.Stock = newStock;
+            }
+            else
+            {
+                throw new Exception("Produsul nu a fost gasit.");
+            }
+        }
+        public void SalveazaComanda(Comanda comanda)
+        {
+            if (comanda != null)
+            {
+                Comenzi.Add(comanda);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(comanda), "Comanda nu poate fi null.");
+            }
+        }
+        
     }
 }
